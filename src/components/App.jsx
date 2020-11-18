4ef10204f902a9a0c4d41f548325d596c6209757
RobBennettUK <38293815+RobBennettUK@users.noreply.github.com>
2020-11-18 17:29:57 +0000

    Lesson Completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  // const state = React.useState(123);
+  // Thus we get what is shown below instead by deconstruction (so we don't need to use state[0] and state[1])
+  // We can define the names ourselves
+  // It assigns the value of count to the first element of state (i.e. 123) and the second element (a function which determines what the state is changed to) is named setCount
+  const [count, setCount] = React.useState(123);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
+
+// Whenever the value of state (the first element of the array state[0]), it will re-render
+// This will only update the parts that change
+
+// Destructuring (objects and arrays)
+// This helps you make complex things simple. See below:
+// const rgb = [41, 128, 185];
+// const [red, green, blue] = [41, 128, 185];
+// console.log(blue);
+// console.log(rgb[2]);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-  <div className="container">
-    <h1>0</h1>
-    <button>+</button>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Remember that HTML attributes get converted into camel case
+// Thus on-click becomes onClick
+
+// Remember that despite increasing count, it won't change what we see because we haven't rendered the page again
+// This means we could put the ReactDom back into the function increase
+
+// This gives us:
+// function increase() {
+//   count++;
+//   console.log(count);
+//   ReactDOM.render(
+//     <div className="container">
+//       <h1>{count}</h1>
+//       <button onClick={increase}>+</button>
+//     </div>,
+//     document.getElementById("root")
+//   );
+// }
+
+// This can be solved by using hooks. See the solution on the App page. Hooks can only be used within a function component
